99f1bd135a5160349e07e8a47cd57ac98a717c85
vittiatet <luis.maylle@unmsm.edu.pe>
2023-08-11 22:47:47 -0500

    hola, esto será un juego sencillo

---
+++ src/t_001/T_001.java
@@ -0,0 +1,15 @@
+
+package t_001;
+
+/**
+ *
+ * @author vitiate
+ */
+public class T_001 {
+
+    public static void main(String[] args) {
+        // BAD LAND
+        System.out.println("hello world");
+    }
+    
+}
